56bc4d18d7e634552b90aaf539b9e65db4ed7981
JeonJW24 <tyui153@naver.com>
2025-07-04 01:14:30 +0900

    feat: 회원가입 시 비밀번호 확인 필드 추가

--- src/main/java/com/grepp/funfun/app/controller/api/user/payload/SignupRequest.java
+++ src/main/java/com/grepp/funfun/app/controller/api/user/payload/SignupRequest.java
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import com.grepp.funfun.app.model.auth.code.Role;
 import com.grepp.funfun.app.model.user.code.Gender;
 import com.grepp.funfun.app.model.user.entity.User;
+import jakarta.validation.constraints.AssertTrue;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
@@ -23,6 +24,13 @@ public class SignupRequest {
         message = "비밀번호는 최소 8자리 이상에서 최대 20자리 이하의 영문자, 숫자, 특수문자 조합으로 이루어져야 합니다."
     )
     private String password;
+
+    private String confirmPassword;
+
+    @AssertTrue(message = "비밀번호가 서로 일치하지 않습니다.")
+    public boolean isConfirmPassword() {
+        return password.equals(confirmPassword);
+    }
 
     @NotBlank(message = "닉네임은 필수입니다.")
     private String nickname;
