de6a6341f5b99abae6ba399d9cd69fda2e9cd774
Park <ox2789@naver.com>
2025-07-07 22:01:47 +0900

    fix: 그룹 상태 관리를 위한 값 추가

--- src/main/java/com/grepp/funfun/app/model/group/code/GroupStatus.java
+++ src/main/java/com/grepp/funfun/app/model/group/code/GroupStatus.java
@@ -1,12 +1,15 @@
 package com.grepp.funfun.app.model.group.code;
 
+import lombok.Getter;
 
+@Getter
 public enum GroupStatus {
 
     RECRUITING("모집 중"),
     FULL("모집 마감"),
     COMPLETED("모임 완료"),
-    CANCELED("모임 취소");
+    CANCELED("모임 취소"),
+    DELETE("모임 삭제");
 
     private final String koreanName;
 
@@ -14,7 +17,4 @@ public enum GroupStatus {
         this.koreanName = koreanName;
     }
 
-    public String getKoreanName() {
-        return koreanName;
-    }
 }
