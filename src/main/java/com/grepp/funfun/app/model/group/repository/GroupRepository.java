a4abac68c5885a95cb7cd70f2e101dd83cc7127f
Park <ox2789@naver.com>
2025-07-07 22:02:08 +0900

    feat: 모임 기능 구현

--- src/main/java/com/grepp/funfun/app/model/group/repository/GroupRepository.java
+++ src/main/java/com/grepp/funfun/app/model/group/repository/GroupRepository.java
@@ -2,6 +2,7 @@ package com.grepp.funfun.app.model.group.repository;
 
 import com.grepp.funfun.app.model.group.entity.Group;
 import com.grepp.funfun.app.model.user.entity.User;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 
@@ -9,6 +10,9 @@ public interface GroupRepository extends JpaRepository<Group, Long>, GroupReposi
 
     Group findFirstByLeader(User user);
 
+    List<Group> findByActivatedTrue();
+
+    List<Group> findByEmailAndActivatedTrue(String email);
 
 
 }
--- src/main/java/com/grepp/funfun/app/model/group/service/GroupService.java
+++ src/main/java/com/grepp/funfun/app/model/group/service/GroupService.java
@@ -1,12 +1,14 @@
 package com.grepp.funfun.app.model.group.service;
 
 import com.grepp.funfun.app.controller.api.group.payload.GroupRequest;
+import com.grepp.funfun.app.controller.api.group.payload.GroupResponse;
 import com.grepp.funfun.app.model.bookmark.entity.GroupBookmark;
 import com.grepp.funfun.app.model.bookmark.repository.GroupBookmarkRepository;
 import com.grepp.funfun.app.model.calendar.entity.Calendar;
 import com.grepp.funfun.app.model.calendar.repository.CalendarRepository;
 import com.grepp.funfun.app.model.chat.entity.ChatRoom;
 import com.grepp.funfun.app.model.chat.repository.ChatRoomRepository;
+import com.grepp.funfun.app.model.group.code.GroupStatus;
 import com.grepp.funfun.app.model.group.dto.GroupDTO;
 import com.grepp.funfun.app.model.group.entity.Group;
 import com.grepp.funfun.app.model.group.entity.GroupHashtag;
@@ -21,6 +23,7 @@ import com.grepp.funfun.app.model.user.repository.UserRepository;
 import com.grepp.funfun.infra.error.exceptions.CommonException;
 import com.grepp.funfun.infra.response.ResponseCode;
 import com.grepp.funfun.util.ReferencedWarning;
+import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
@@ -47,22 +50,38 @@ public class GroupService {
     public List<GroupDTO> findAll() {
         final List<Group> groups = groupRepository.findAll(Sort.by("id"));
         return groups.stream()
-                .map(group -> mapToDTO(group, new GroupDTO()))
-                .toList();
+            .map(group -> mapToDTO(group, new GroupDTO()))
+            .toList();
+    }
+    // 모든 모임 조회
+    @Transactional(readOnly = true)
+    public List<GroupResponse> findByActivatedTrue(){
+        return groupRepository.findByActivatedTrue().stream()
+            .map(this::mapToResponse)
+            .collect(Collectors.toList());
     }
 
     public GroupDTO get(final Long id) {
         return groupRepository.findById(id)
-                .map(group -> mapToDTO(group, new GroupDTO()))
-                .orElseThrow(() -> new CommonException(ResponseCode.NOT_FOUND));
+            .map(group -> mapToDTO(group, new GroupDTO()))
+            .orElseThrow(() -> new CommonException(ResponseCode.NOT_FOUND));
     }
+
+    // 내가 속한 모임 모든 모임을 조회(for 채팅)
+    @Transactional(readOnly = true)
+    public List<GroupResponse> getMyGroups(String userEmail){
+        return groupRepository.findByEmailAndActivatedTrue(userEmail).stream()
+            .map(this::mapToResponse)
+            .collect(Collectors.toList());
+    }
+
     // 모임 생성
     @Transactional
-    public void create(String leaderEmail,GroupRequest request) {
+    public void create(String leaderEmail, GroupRequest request) {
 
         User leader = userRepository.findByEmail(leaderEmail);
 
-        if(leader == null || !leader.getActivated()){
+        if (leader == null || !leader.getActivated()) {
             throw new CommonException(ResponseCode.NOT_FOUND);
         }
         Group savedGroup = groupRepository.save(request.toEntity(leader));
@@ -92,59 +111,136 @@ public class GroupService {
         // 모임 생성 시 자동으로 팀 채팅방 생성하기
         ChatRoom chatRoom = new ChatRoom();
         chatRoom.setGroup(savedGroup);
-        chatRoom.setName(savedGroup.getId()+"번 채팅방");
+        chatRoom.setName(savedGroup.getId() + "번 채팅방");
         chatRoomRepository.save(chatRoom);
     }
-    //모임 참여 신청
+
+
+    // 모임 수정
     @Transactional
-    public void apply(Long groupId, String userEmail){
-        // 모임[모집중, True]
-        Group group = groupRepository.findActiveRecruitingGroup(groupId)
-            .orElseThrow(()-> new CommonException(ResponseCode.NOT_FOUND));
+    public void update(Long groupId, String leaderEmail, GroupRequest updateRequest) {
+        Group group = groupWithLeaderValidation(groupId, leaderEmail);
 
-        // 사용자 검증
-        User user = userRepository.findByEmail(userEmail);
-        if(user == null){
-            throw new CommonException(ResponseCode.NOT_FOUND);
-        }
+        //Builder 패턴으로 내용 수정하고 다시 저장하기
+        group.setTitle(updateRequest.getTitle());
+        group.setExplain(updateRequest.getExplain());
+        group.setPlaceName(updateRequest.getPlaceName());
+        group.setGroupDate(updateRequest.getGroupDate());
+        group.setAddress(updateRequest.getAddress());
+        group.setCategory(updateRequest.getCategory());
+        group.setMaxPeople(updateRequest.getMaxPeople());
+        group.setLatitude(updateRequest.getLatitude());
+        group.setLongitude(updateRequest.getLongitude());
+        group.setDuring(updateRequest.getDuring());
 
-        // 리더는 신청 불가하도록 검증
-        if(group.getLeader().getEmail().equals(user.getEmail())){
-            throw new CommonException(ResponseCode.BAD_REQUEST);
-        }
-
-        // 중복 신청
-        boolean alreadyApplied = participantRepository.existsByUserAndGroup(user,group);
-        if(alreadyApplied){
-            throw new CommonException(ResponseCode.BAD_REQUEST);
-        }
-
-//        // 모임 시간 체크
-//        if(group.getGroupDate().isBefore(LocalDateTime.now())){
-//            throw new CommonException(ResponseCode.BAD_REQUEST);
-//        }
-
-        // 참여자 생성
-        Participant participant = Participant.builder()
-            .user(user)
-            .group(group)
-            .role(ParticipantRole.MEMBER)
-            .status(ParticipantStatus.PENDING)
-            .build();
-
-        participantRepository.save(participant);
+        // 재설정
+        group.getHashtags().clear();
+        List<GroupHashtag> hashtags = updateRequest.getHashTags().stream()
+            .map(hashtagName -> GroupHashtag.builder()
+                .tag(hashtagName)
+                .group(group)
+                .build())
+            .collect(Collectors.toList());
+        group.setHashtags(hashtags);
 
     }
 
-    public void update(final Long id, final GroupDTO groupDTO) {
-        final Group group = groupRepository.findById(id)
-                .orElseThrow(() -> new CommonException(ResponseCode.NOT_FOUND));
-        mapToEntity(groupDTO, group);
-        groupRepository.save(group);
+    // 모임 삭제
+    @Transactional
+    public void delete(Long groupId, String leaderEmail) {
+        Group group = groupWithLeaderValidation(groupId, leaderEmail);
+
+        // 소프트 삭제 방식으로 -> false / DELETE 로 변경
+        group.unActivated();
+        group.setStatus(GroupStatus.DELETE);
+
+        // 관련 멤버들 active - false / status - GROUP_DELETED
+        for (Participant participant : group.getParticipants()) {
+            participant.unActivated();
+            participant.setStatus(ParticipantStatus.GROUP_DELETED);
+
+        }
     }
 
-    public void delete(final Long id) {
-        groupRepository.deleteById(id);
+    //모임 취소
+    @Transactional
+    public void cancel(Long groupId, String leaderEmail) {
+        Group group = groupWithLeaderValidation(groupId, leaderEmail);
+
+        // 소프트 삭제 방식으로 -> false / CANCEL 로 변경
+        group.unActivated();
+        group.setStatus(GroupStatus.CANCELED);
+
+        // 관련 멤버들 active - false / status - GROUP_CANCELED
+        for (Participant participant : group.getParticipants()) {
+            participant.unActivated();
+            participant.setStatus(ParticipantStatus.GROUP_CANCELED);
+
+        }
+    }
+
+    // 모임 완료
+    @Transactional
+    public void complete(Long groupId, String leaderEmail) {
+        Group group = groupWithLeaderValidation(groupId, leaderEmail);
+
+        if (!Arrays.asList(GroupStatus.FULL, GroupStatus.RECRUITING).contains(group.getStatus())) {
+            throw new IllegalStateException("완료할 수 없는 모임 상태입니다.");
+        }
+
+        group.unActivated();
+        group.setStatus(GroupStatus.COMPLETED);
+
+        // 관련 멤버들 active - false / status - GROUP_CANCELED
+        for (Participant participant : group.getParticipants()) {
+            participant.unActivated();
+            participant.setStatus(ParticipantStatus.GROUP_COMPLETE);
+        }
+
+    }
+    // 그룹에 리더이자 사용자 false 상태 확인 코드(중복 코드 대체)
+    private Group groupWithLeaderValidation(Long groupId, String leaderEmail) {
+        // 그룹 존재 확인
+        Group group = groupRepository.findById(groupId)
+            .orElseThrow(() -> new CommonException(ResponseCode.NOT_FOUND));
+        // 리더인지 확인
+        User leader = group.getLeader();
+        if (!leader.getEmail().equals(leaderEmail)) {
+            throw new CommonException(ResponseCode.UNAUTHORIZED);
+        }
+        // 사용자로 false (정지 당한 사람이 아닌지)
+        if (!leader.getActivated()) {
+            throw new CommonException(ResponseCode.UNAUTHORIZED);
+        }
+        return group;
+    }
+
+    private GroupResponse mapToResponse(final Group group) {
+        GroupResponse response = new GroupResponse();
+
+        response.setId(group.getId());
+        response.setTitle(group.getTitle());
+        response.setExplain(group.getExplain());
+        response.setPlaceName(group.getPlaceName());
+        response.setAddress(group.getAddress());
+        response.setGroupDate(group.getGroupDate());
+        response.setMaxPeople(group.getMaxPeople());
+        response.setNowPeople(group.getNowPeople());
+        response.setStatus(group.getStatus());
+        response.setLatitude(group.getLatitude());
+        response.setLongitude(group.getLongitude());
+        response.setDuring(group.getDuring());
+        response.setCategory(group.getCategory());
+        response.setLeader(group.getLeader() == null ? null : group.getLeader().getEmail());
+        response.setActivated(group.getActivated());
+
+        // 해시태그 변환
+        List<String> hashTags = group.getHashtags().stream()
+            .map(GroupHashtag::getTag)
+            .collect(Collectors.toList());
+        response.setHashTags(hashTags);
+
+        return response;
     }
 
     private GroupDTO mapToDTO(final Group group, final GroupDTO groupDTO) {
@@ -178,7 +274,8 @@ public class GroupService {
         group.setLongitude(groupDTO.getLongitude());
         group.setDuring(groupDTO.getDuring());
         group.setCategory(groupDTO.getCategory());
-        final User leader = groupDTO.getLeader() == null ? null : userRepository.findById(groupDTO.getLeader())
+        final User leader =
+            groupDTO.getLeader() == null ? null : userRepository.findById(groupDTO.getLeader())
                 .orElseThrow(() -> new CommonException(ResponseCode.NOT_FOUND));
         group.setLeader(leader);
         return group;
@@ -187,7 +284,7 @@ public class GroupService {
     public ReferencedWarning getReferencedWarning(final Long id) {
         final ReferencedWarning referencedWarning = new ReferencedWarning();
         final Group group = groupRepository.findById(id)
-                .orElseThrow(() -> new CommonException(ResponseCode.NOT_FOUND));
+            .orElseThrow(() -> new CommonException(ResponseCode.NOT_FOUND));
         final GroupBookmark groupGroupBookmark = groupBookmarkRepository.findFirstByGroup(group);
         if (groupGroupBookmark != null) {
             referencedWarning.setKey("group.groupBookmark.group.referenced");
