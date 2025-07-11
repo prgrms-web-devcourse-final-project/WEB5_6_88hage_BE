672a16f9e5b17592acb5f5613ec7ac9b08408106
JeonJW24 <tyui153@naver.com>
2025-07-11 16:58:00 +0900

    fix: OAuth2 로직, 경로 수정

--- src/main/java/com/grepp/funfun/app/infra/auth/oauth2/OAuth2SuccessHandler.java
+++ src/main/java/com/grepp/funfun/app/infra/auth/oauth2/OAuth2SuccessHandler.java
@@ -3,6 +3,8 @@ package com.grepp.funfun.app.infra.auth.oauth2;
 import com.grepp.funfun.app.domain.auth.service.AuthService;
 import com.grepp.funfun.app.domain.auth.vo.Role;
 import com.grepp.funfun.app.domain.auth.dto.TokenDto;
+import com.grepp.funfun.app.domain.preference.repository.ContentPreferenceRepository;
+import com.grepp.funfun.app.domain.preference.repository.GroupPreferenceRepository;
 import com.grepp.funfun.app.domain.user.entity.User;
 import com.grepp.funfun.app.domain.user.entity.UserInfo;
 import com.grepp.funfun.app.domain.user.repository.UserInfoRepository;
@@ -15,6 +17,7 @@ import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.authentication.SimpleUrlAuthenticationSuccessHandler;
@@ -28,7 +31,12 @@ public class OAuth2SuccessHandler extends SimpleUrlAuthenticationSuccessHandler 
     private final AuthService authService;
     private final UserRepository userRepository;
     private final UserInfoRepository userInfoRepository;
+    private final ContentPreferenceRepository contentPreferenceRepository;
+    private final GroupPreferenceRepository groupPreferenceRepository;
     private final PasswordEncoder passwordEncoder;
+
+    @Value("${front-server.domain}")
+    private String front;
 
     @Override
     public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response,
@@ -61,25 +69,27 @@ public class OAuth2SuccessHandler extends SimpleUrlAuthenticationSuccessHandler 
             TokenCookieFactory.setAllAuthCookies(response, tokenDto);
 
             // NOTE : 프론트 측 URI 로 변경 필요
-            response.sendRedirect("/users/oauth2/signup");
+            response.sendRedirect(front + "/users/oauth2/signup");
 
         } else {
             User user = userRepository.findById(email).orElse(null);
-            if (user != null && user.getRole().equals(Role.ROLE_GUEST)) {
-                // NOTE : 프론트 측 URI 로 변경 필요
-                response.sendRedirect("/users/oauth2/signup");
-            }
 
             if (user != null) {
                 TokenDto tokenDto = authService.processTokenSignin(email, user.getRole().name(), false);
 
                 TokenCookieFactory.setAllAuthCookies(response, tokenDto);
 
-                if (user.getGroupPreferences().isEmpty() || user.getContentPreferences().isEmpty()) {
+                if (user.getRole().equals(Role.ROLE_GUEST)) {
                     // NOTE : 프론트 측 URI 로 변경 필요
-                    response.sendRedirect("/users/preference");
+                    response.sendRedirect(front + "/users/oauth2/signup");
+                    return;
+                }
+
+                if (groupPreferenceRepository.findByUserEmail(email).isEmpty() || contentPreferenceRepository.findByUserEmail(email).isEmpty()) {
+                    // NOTE : 프론트 측 URI 로 변경 필요
+                    response.sendRedirect(front + "/users/preference");
                 } else {
-                    response.sendRedirect("/");
+                    response.sendRedirect(front + "/");
                 }
             }
         }
