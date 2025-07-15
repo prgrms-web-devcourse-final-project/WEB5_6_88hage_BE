5dfcfda5f7938e5b6afa0571e6c2ce05870e8b7e
Kimoon J <jkm3093@naver.com>
2025-07-15 16:19:24 +0900

    test: 컨텐츠, 모임 관련 데이터 초기화 테스트

--- src/test/java/com/grepp/funfun/app/infra/init/DataInitializeServiceTest.java
+++ src/test/java/com/grepp/funfun/app/infra/init/DataInitializeServiceTest.java
@@ -1,11 +1,17 @@
 package com.grepp.funfun.app.infra.init;
 
+import com.grepp.funfun.app.domain.content.entity.Content;
+import com.grepp.funfun.app.domain.content.repository.ContentRepository;
+import java.util.List;
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+@Slf4j
 @SpringBootTest
 class DataInitializeServiceTest {
 
@@ -13,8 +19,23 @@ class DataInitializeServiceTest {
     @Autowired
     DataInitializeService dataInitializeService;
 
+    @Autowired
+    ContentRepository contentRepository;
+
     @Test
     public void initTest() {
         dataInitializeService.initializeVector();
     }
+
+
+    @Test
+    @Transactional
+    public void search() {
+        List<Content> contents = contentRepository.findAll();
+
+        log.info("테스트 결과 {}",contents.getFirst().getCategory().getCategory());
+
+    }
+
+
 }
