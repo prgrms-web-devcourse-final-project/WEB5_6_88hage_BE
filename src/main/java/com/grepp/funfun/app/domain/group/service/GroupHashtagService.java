f17c5a79c0a58add40b5a1a4da8c026fa660d27b
Park <ox2789@naver.com>
2025-07-16 12:47:38 +0900

    feat: 자동 완성 기능(with Redis)

--- src/main/java/com/grepp/funfun/app/domain/group/service/GroupHashtagService.java
+++ src/main/java/com/grepp/funfun/app/domain/group/service/GroupHashtagService.java
@@ -7,34 +7,57 @@ import com.grepp.funfun.app.domain.group.repository.GroupHashtagRepository;
 import com.grepp.funfun.app.domain.group.repository.GroupRepository;
 import com.grepp.funfun.app.infra.error.exceptions.CommonException;
 import com.grepp.funfun.app.infra.response.ResponseCode;
+import java.util.Collections;
 import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Sort;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
 
-
 @Service
+@RequiredArgsConstructor
 public class GroupHashtagService {
 
     private final GroupHashtagRepository groupHashtagRepository;
     private final GroupRepository groupRepository;
+    private final RedisTemplate<String, Object> redisTemplate;
 
-    public GroupHashtagService(final GroupHashtagRepository groupHashtagRepository,
-            final GroupRepository groupRepository) {
-        this.groupHashtagRepository = groupHashtagRepository;
-        this.groupRepository = groupRepository;
+
+    // todo : prefix 를 통해 값 찾아 가져오기 : set prefix tier
+    // 값 저장
+    public void saveWord(String word) {
+        for (int i = 1; i <= word.length(); i++) {
+            String prefix = word.substring(0, i);
+            String key = "autocomplete:" + prefix;
+            redisTemplate.opsForSet().add(key, word);
+        }
+    }
+
+    // 자동 완성
+    public Set<String> getAutoCompleteWord(String prefix) {
+        String key = "autocomplete:" + prefix;
+        Set<Object> results = redisTemplate.opsForSet().members(key);
+
+        // 값이 없을 경우
+        if(results == null||results.isEmpty()) {
+            return Collections.emptySet();
+        }
+        return results.stream().map(Object::toString).collect(Collectors.toSet());
     }
 
     public List<GroupHashtagDTO> findAll() {
         final List<GroupHashtag> groupHashtags = groupHashtagRepository.findAll(Sort.by("id"));
         return groupHashtags.stream()
-                .map(groupHashtag -> mapToDTO(groupHashtag, new GroupHashtagDTO()))
-                .toList();
+            .map(groupHashtag -> mapToDTO(groupHashtag, new GroupHashtagDTO()))
+            .toList();
     }
 
     public GroupHashtagDTO get(final Long id) {
         return groupHashtagRepository.findById(id)
-                .map(groupHashtag -> mapToDTO(groupHashtag, new GroupHashtagDTO()))
-                .orElseThrow(() -> new CommonException(ResponseCode.NOT_FOUND));
+            .map(groupHashtag -> mapToDTO(groupHashtag, new GroupHashtagDTO()))
+            .orElseThrow(() -> new CommonException(ResponseCode.NOT_FOUND));
     }
 
     public Long create(final GroupHashtagDTO groupHashtagDTO) {
@@ -45,7 +68,7 @@ public class GroupHashtagService {
 
     public void update(final Long id, final GroupHashtagDTO groupHashtagDTO) {
         final GroupHashtag groupHashtag = groupHashtagRepository.findById(id)
-                .orElseThrow(() -> new CommonException(ResponseCode.NOT_FOUND));
+            .orElseThrow(() -> new CommonException(ResponseCode.NOT_FOUND));
         mapToEntity(groupHashtagDTO, groupHashtag);
         groupHashtagRepository.save(groupHashtag);
     }
@@ -55,7 +78,7 @@ public class GroupHashtagService {
     }
 
     private GroupHashtagDTO mapToDTO(final GroupHashtag groupHashtag,
-            final GroupHashtagDTO groupHashtagDTO) {
+        final GroupHashtagDTO groupHashtagDTO) {
         groupHashtagDTO.setId(groupHashtag.getId());
         groupHashtagDTO.setTag(groupHashtag.getTag());
         groupHashtagDTO.setGroup(groupHashtag.getGroup() == null ? null : groupHashtag.getGroup().getId());
@@ -63,10 +86,10 @@ public class GroupHashtagService {
     }
 
     private GroupHashtag mapToEntity(final GroupHashtagDTO groupHashtagDTO,
-            final GroupHashtag groupHashtag) {
+        final GroupHashtag groupHashtag) {
         groupHashtag.setTag(groupHashtagDTO.getTag());
         final Group group = groupHashtagDTO.getGroup() == null ? null : groupRepository.findById(groupHashtagDTO.getGroup())
-                .orElseThrow(() -> new CommonException(ResponseCode.NOT_FOUND));
+            .orElseThrow(() -> new CommonException(ResponseCode.NOT_FOUND));
         groupHashtag.setGroup(group);
         return groupHashtag;
     }
