d017cbf4d8a51f3a06e0fad4b41044812c6c06ad
Park <ox2789@naver.com>
2025-07-18 01:02:35 +0900

    chore: 즐겨찾기 기능 삭제

--- src/main/java/com/grepp/funfun/app/domain/bookmark/repository/GroupBookmarkRepositoryCustom.java
+++
@@ -1,9 +0,0 @@
-package com.grepp.funfun.app.domain.bookmark.repository;
-
-import com.grepp.funfun.app.domain.bookmark.entity.GroupBookmark;
-import java.util.List;
-
-public interface GroupBookmarkRepositoryCustom {
-    List<GroupBookmark> getMyGroupBookMarks(String userEmail);
-
-}
