f8fcdd948ad82cf4bbe7e7d4e5e4b7157c48bc5d
Irreplaceable-j <jkm3093@naver.com>
2025-07-24 18:55:06 +0900

    chore: metadata 필드 이름 설정

--- src/main/java/com/grepp/funfun/app/infra/config/RecommendationConfig.java
+++ src/main/java/com/grepp/funfun/app/infra/config/RecommendationConfig.java
@@ -10,68 +10,15 @@ import dev.langchain4j.rag.content.retriever.EmbeddingStoreContentRetriever;
 import dev.langchain4j.store.embedding.EmbeddingStore;
 import dev.langchain4j.store.embedding.mongodb.IndexMapping;
 import dev.langchain4j.store.embedding.mongodb.MongoDbEmbeddingStore;
+import java.time.LocalDateTime;
 import java.util.HashSet;
+import java.util.Set;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class RecommendationConfig {
-
-//    @Bean(name = "chatModel")
-//    public ChatLanguageModel chatModel() {
-//        return GoogleAiGeminiChatModel.builder()
-//            .apiKey("AIzaSyCoJ8iF8VWMM084BiTUFhZtcYZFdYZNdKI")
-//            .modelName("gemini-2.0-flash-lite")
-//            .logRequestsAndResponses(true)
-//            .maxOutputTokens(1000)
-//            .build();
-//    }
-//
-//    // 임베딩 모델 정의
-//    // 문장 -> 벡터로
-//    @Bean
-//    public EmbeddingModel embeddingModel() {
-//        return new AllMiniLmL6V2EmbeddingModel();
-//    }
-//
-//    // MongoDB 벡터 DB로 사용하기 위한 설정
-//    @Bean
-//    public MongoDbEmbeddingStore embeddingStore(EmbeddingModel embeddingModel,
-//        MongoClient mongoClient) {
-//
-//        Boolean createIndex = true;
-//        IndexMapping indexMapping = IndexMapping.builder()
-//            .dimension(embeddingModel.dimension())
-//            .metadataFieldNames(new HashSet<>())
-//            .build();
-//
-//        // 벡터 기반 RAG/추천 시스템이 동작할 수 있게 builder 패턴으로 생성
-//        return MongoDbEmbeddingStore.builder()
-//            .databaseName("llm")
-//            .collectionName("restaurants")
-//            .createIndex(createIndex)
-//            .indexName("vector_index")
-//            .indexMapping(indexMapping)
-//            .fromClient(mongoClient)
-//            .build();
-//    }
-//
-//    // RAG 방식 검색, 추천 시스템, 유사 문장 검색 활용하기 위한 컴포넌트
-//    @Bean
-//    EmbeddingStoreContentRetriever embeddingStoreContentRetriever(
-//        EmbeddingStore<TextSegment> embeddingStore,
-//        EmbeddingModel embeddingModel
-//    ){
-//        return EmbeddingStoreContentRetriever.builder()
-//            .embeddingStore(embeddingStore)
-//            .embeddingModel(embeddingModel)
-//            .maxResults(100)
-//            .minScore(0.7)
-//            .build();
-//    }
-//
-//}
 
     // 임베딩 모델 정의
     // 문장 -> 벡터로
@@ -86,9 +33,15 @@ public class RecommendationConfig {
         MongoClient mongoClient) {
 
         Boolean createIndex = true;
+
+        // 메타데이터 필드 이름 설정
+        Set<String> metadataFields = Set.of(
+            "contentTitle", "address", "category", "startDate", "endDate"
+        );
+
         IndexMapping indexMapping = IndexMapping.builder()
                                                 .dimension(embeddingModel.dimension())
-                                                .metadataFieldNames(new HashSet<>())
+                                                .metadataFieldNames(metadataFields)
                                                 .build();
 
         // 벡터 기반 RAG/추천 시스템이 동작할 수 있게 builder 패턴으로 생성
@@ -148,5 +101,4 @@ public class RecommendationConfig {
                                              .minScore(0.6)
                                              .build();
     }
-
 }
