2ecdd64a90b161fb900a810d4c73e74d75119a09
hesador12 <97518677+hesador12@users.noreply.github.com>
2025-07-25 19:57:30 +0900

    refactor: admin user 관련 api - swagger operation 추가

--- src/main/java/com/grepp/funfun/app/domain/admin/controller/AdminUserApiController.java
+++ src/main/java/com/grepp/funfun/app/domain/admin/controller/AdminUserApiController.java
@@ -7,6 +7,7 @@ import com.grepp.funfun.app.infra.error.exceptions.CommonException;
 import com.grepp.funfun.app.infra.response.ApiResponse;
 import com.grepp.funfun.app.infra.response.ResponseCode;
 
+import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
@@ -29,6 +30,7 @@ public class AdminUserApiController {
 
     // 유저 전체 조회
     @GetMapping
+    @Operation(summary= "유저 전체 조회", description = "모든 유저를 조회힙나디.")
     public ResponseEntity<ApiResponse<List<UserDTO>>> getAllUsers(Authentication authentication) {
         UserDTO admin = adminUserService.getAuthenticatedUser(authentication);
         checkAdminRole(admin);
@@ -38,6 +40,7 @@ public class AdminUserApiController {
     }
 
     @GetMapping("/{email}")
+    @Operation(summary= "유저 상세 조회", description = "특정 유저를 상세 조회합니다.")
     public ResponseEntity<ApiResponse<UserDTO>> getUser(@PathVariable String email, Authentication authentication) {
         UserDTO admin = adminUserService.getAuthenticatedUser(authentication);
         checkAdminRole(admin);
@@ -49,6 +52,7 @@ public class AdminUserApiController {
     // 유저 제재 - 7일, 30일, 영구정지
     // @param duration: 7, 30, -1
     @PatchMapping("/{email}/suspend")
+    @Operation(summary= "유저 제재 적용", description= "특정 유저에게 7, 30일 일시정지 또는 영구정지(-1)조치를 취합니다.")
     public ResponseEntity<ApiResponse<String>> suspendUser(
             @PathVariable String email,
             @RequestParam int duration,
@@ -63,6 +67,7 @@ public class AdminUserApiController {
 
     // 닉네임 유저 검색
     @GetMapping("/nickname/{nickname}")
+    @Operation(summary= "닉네임으로 유저 검색", description= "닉네임으로 특정 유저를 검색합니다.")
     public ResponseEntity<ApiResponse<UserDTO>> getIUserByNickname(@PathVariable String nickname,
                                                                    Authentication authentication) {
         UserDTO admin = adminUserService.getAuthenticatedUser(authentication);
@@ -74,6 +79,7 @@ public class AdminUserApiController {
 
     // 부적절한 닉네임 임의 숫자값 변경
     @PostMapping("/{email}/randomize-nickname")
+    @Operation(summary= "부적절한 닉네임 임의 숫자값 부여", description = "유저의 닉네임을 임의의 8자리 숫자값으로 수정합니다.")
     public ResponseEntity<ApiResponse<String>> randomizeNickname(
             @PathVariable String email,
             Authentication authentication) {
