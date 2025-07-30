b86e3ff20141339b7eb503dcfa46634f6245ad8c
JeonJW24 <tyui153@naver.com>
2025-07-30 13:58:18 +0900

    chore: 불필요한 코드 삭제

--- src/main/java/com/grepp/funfun/app/domain/preference/repository/ContentPreferenceRepository.java
+++ src/main/java/com/grepp/funfun/app/domain/preference/repository/ContentPreferenceRepository.java
@@ -1,14 +1,11 @@
 package com.grepp.funfun.app.domain.preference.repository;
 
 import com.grepp.funfun.app.domain.preference.entity.ContentPreference;
-import com.grepp.funfun.app.domain.user.entity.User;
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 
 public interface ContentPreferenceRepository extends JpaRepository<ContentPreference, Long> {
-
-    ContentPreference findFirstByUser(User user);
 
     void deleteAllByUserEmail(String email);
 
